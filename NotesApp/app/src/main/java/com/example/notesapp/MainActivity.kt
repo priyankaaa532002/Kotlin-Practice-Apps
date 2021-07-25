7f9572562f30a26cc39e3e3e77cdde76a4fcca3e
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2021-07-25 20:26:26 +0530

    na updated

--- NotesApp/app/src/main/java/com/example/notesapp/MainActivity.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.notesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -30,6 +31,15 @@ class MainActivity : AppCompatActivity(), INotesRVAdapter {
     }
 
     override fun onItemClicked(note: Note) {
-        viewModel.deleteNode(note)
+        viewModel.deleteNote(note)
+        Toast.makeText(this,"${note.text} Deleted",Toast.LENGTH_LONG).show()
+    }
+
+    fun submitData(view: View) {
+        val noteText = input.text.toString()
+        if (noteText.isNotEmpty()){
+            viewModel.insertNote(Note(noteText))
+            Toast.makeText(this,"${noteText} Inserted",Toast.LENGTH_LONG).show()
+        }
     }
 }
