ad65cac2d6e9aa6a673967869dabe4879defcfe6
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2021-09-03 17:47:16 +0530

    students data updated

--- NotesApp/app/src/main/java/com/example/notesapp/NoteDataBase.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/NoteDataBase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 
 //Step DataBase
-@Database(entities = arrayOf(Note::class),version = 1,exportSchema = false)
+@Database(entities = [Note::class],version = 1,exportSchema = false)
 abstract class NoteDataBase : RoomDatabase(){
 
     abstract fun getNoteDao() : NoteDao
